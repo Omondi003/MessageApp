077ac25b67fef2a153fb3526ad0a6d30ecbffaa3
Omondi003 <joelomondi2002@gmail.com>
2024-04-20 23:08:36 +0300

    Initializing the message app and rendering the two views

---
+++ routes/index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+
+const messages=[
+   {
+    user:'Joel',
+    message:'Heloo Joel',
+    added:new Date()
+   },
+
+   {
+     user: 'Peter',
+     message: 'Heloo Peter',
+     added: new Date()
+   },
+
+   {
+     user:'Michael',
+     message: "Heloo mr michael",
+     added: new Date()
+   }
+];
+ 
+/* GET home page. */
+router.get('/', (req, res, next)=> {
+  res.render('index', { title: 'MessageApp', messages:messages} );
+});
+
+module.exports = router;
